5f90dc8e78456f235cda41a0d339c1fa255c0e21
Peter Vrenken <github@petervrenken.nl>
2025-08-24 22:33:50 +0200

    Simplify `ToString` output format in `DeclareStatement`.

--- EtAlii.Ubigia.Lark/Syntax/_Model/Statements/DeclareStatement.cs
+++ EtAlii.Ubigia.Lark/Syntax/_Model/Statements/DeclareStatement.cs
@@ -3,4 +3,6 @@
 public record DeclareStatement : Statement
 {
     public required string[] Names { get; init; }
+
+    public override string ToString() => $"%declare {string.Join(' ', Names)}";
 }
