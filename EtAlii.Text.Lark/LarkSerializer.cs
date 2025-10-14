d88769e8d45f906927f2b6df2e682e427406f045
Peter Vrenken <github@petervrenken.nl>
2025-10-14 23:44:53 +0200

    Add `LarkSerializer` with basic skeleton for serialization and deserialization

---
+++ EtAlii.Text.Lark/LarkSerializer.cs
@@ -0,0 +1,14 @@
+namespace EtAlii.Text.Lark;
+
+public static class LarkSerializer
+{
+    public static void Serialize<T>(T instance, LarkSerializationContext context)
+    {
+        
+    }
+
+    public static T Deserialize<T>(object lark, LarkSerializationContext context)
+    {
+        throw new NotImplementedException();
+    }
+}
