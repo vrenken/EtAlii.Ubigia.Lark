eacdffde90b8a85e513e9df70aac3e7365ab0f9c
Peter Vrenken <github@petervrenken.nl>
2025-10-14 17:06:55 +0200

    Add `StreamingChatToolCallsBuilder` and `SequenceBuilder` to handle streaming chat tool call updates
    Implement `StreamingChatToolCallsBuilder` for managing and building streaming chat tool call updates. Add `SequenceBuilder` to accumulate and construct read-only sequences for function arguments efficiently.

---
+++ EtAlii.Ubigia.OpenAI.Tests/SequenceBuilder.cs
@@ -0,0 +1,64 @@
+using System.Buffers;
+using System.Diagnostics;
+
+namespace EtAlii.Ubigia.OpenAI.Tests;
+
+public class SequenceBuilder<T>
+{
+    private Segment _first = null!;
+    private Segment _last = null!;
+
+    public void Append(ReadOnlyMemory<T> data)
+    {
+        if (_first == null!)
+        {
+            Debug.Assert(_last == null);
+            _first = new Segment(data);
+            _last = _first;
+        }
+        else
+        {
+            _last = _last.Append(data);
+        }
+    }
+
+    public ReadOnlySequence<T> Build()
+    {
+        if (_first == null!)
+        {
+            Debug.Assert(_last == null);
+            return ReadOnlySequence<T>.Empty;
+        }
+
+        if (_first == _last)
+        {
+            Debug.Assert(_first.Next == null);
+            return new ReadOnlySequence<T>(_first.Memory);
+        }
+
+        return new ReadOnlySequence<T>(_first, 0, _last, _last.Memory.Length);
+    }
+    
+    internal sealed class Segment : ReadOnlySequenceSegment<T>
+    {
+        public Segment(ReadOnlyMemory<T> items) : this(items, 0)
+        {
+        }
+
+        private Segment(ReadOnlyMemory<T> items, long runningIndex)
+        {
+            Debug.Assert(runningIndex >= 0);
+            Memory = items;
+            RunningIndex = runningIndex;
+        }
+
+        public Segment Append(ReadOnlyMemory<T> items)
+        {
+            long runningIndex;
+            checked { runningIndex = RunningIndex + Memory.Length; }
+            Segment segment = new(items, runningIndex);
+            Next = segment;
+            return segment;
+        }
+    }
+}
