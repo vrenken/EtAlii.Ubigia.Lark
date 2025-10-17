3f5a944312287b834f12579b48108852232ee262
Peter Vrenken <github@petervrenken.nl>
2025-10-17 13:39:32 +0200

    Add `ChatToolExtensionProperties` record for storing instance-specific properties using `ConditionalWeakTable`.

---
+++ EtAlii.OpenAI/_Model/ChatToolExtensionProperties.cs
@@ -0,0 +1,10 @@
+﻿#nullable disable
+namespace EtAlii.OpenAI;
+
+/// <summary>
+/// Small storage record to store new properties to instances using a ConditionalWeakTable instance.  
+/// </summary>
+public record ChatToolExtensionProperties
+{
+    public FunctionToolInvocationInfo Invocation { get; set; } = null!;
+}
