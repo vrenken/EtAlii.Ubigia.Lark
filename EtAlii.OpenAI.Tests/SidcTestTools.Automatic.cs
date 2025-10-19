3b339c44a11cd64658b0a7f2cffe750caea7e4bb
Peter Vrenken <github@petervrenken.nl>
2025-10-19 02:41:33 +0200

    Refactor SIDC test tools:
    - Move manual SIDC refinement logic to `SidcTestTools.Manual`.
    - Reorganize and simplify SIDC-related methods for better readability.
    - Remove redundant implementation from `SidcTestTools.Automatic`.
    - Add `GetSidcRefinementOptionsManual` method and corresponding tool registration.

---
+++ EtAlii.OpenAI.Tests/SidcTestTools.Automatic.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace EtAlii.OpenAI.Tests;
+
+public static partial class SidcTestTools
+{
+    [Description($"Gets the options with which to complement the provided SIDC code with. If you do not know the code then call this method with a blank string. Also Keep calling this function until the result has the '{nameof(SidcRefinementResult.KeepRefining)}' value is to true.")]
+    public static SidcRefinementResult GetSidcRefinementOptionsAutomatic(
+        [Description("The SIDC as it has already been determined, append one of the options to this code.")]
+        string prefix, 
+        [Description("The general textual description of what the final SIDC should represent.")]
+        string hint) => GetSidcRefinementOptionsManual(prefix, hint);
+}
--- EtAlii.OpenAI.Tests/SidcTestTools.Manual.cs
+++ EtAlii.OpenAI.Tests/SidcTestTools.Manual.cs
@@ -1,17 +1,9 @@
-﻿using System.ComponentModel;
-using OpenAI.Chat;
+﻿using OpenAI.Chat;
 
 namespace EtAlii.OpenAI.Tests;
 
-public partial class TestChatCompletionProcessor
+public static partial class SidcTestTools
 {
-    [Description($"Gets the options with which to complement the provided SIDC code with. If you do not know the code then call this method with a blank string. Also Keep calling this function until the result has the '{nameof(SidcRefinementResult.KeepRefining)}' value is to true.")]
-    public static SidcRefinementResult GetSidcRefinementOptionsAutomatic(
-        [Description("The SIDC as it has already been determined, append one of the options to this code.")]
-        string prefix, 
-        [Description("The general textual description of what the final SIDC should represent.")]
-        string hint) => GetSidcRefinementOptionsManual(prefix, hint);
-
     public static SidcRefinementResult GetSidcRefinementOptionsManual(string prefix, string hint)
     {
         switch (prefix.Length)
